2ecf551dfe849052c7ebf84d6914c8cdfe0097b6
Neha Singh <35872919+nehaaa18@users.noreply.github.com>
2020-10-18 18:06:20 +0530

    Add files via upload

---
+++ SubsetSumProblem.java
@@ -0,0 +1,52 @@
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int arr[]={2,3,7,8,10};
+		int n=arr.length;
+		int sum=11;
+		boolean res=sumExists(arr,sum,n);
+		if (res)
+		{
+		    System.out.println("Sum Exists");
+		}
+		else {
+		    System.out.println("Sum Does Not Exists");
+		}
+		
+	}
+	static boolean sumExists(int arr[], int sum,int n)
+	{
+	    if (n==0)
+	    {
+	        return false;
+	    }
+	    boolean mat[][]=new boolean[n+1][sum+1];
+	    for (int i=0;i<n;i++)
+	    {
+	        mat[i][0]=true;
+	    }
+	    for (int j=0;j<=sum;j++)
+	    {
+	        if (j==arr[0])
+	        {
+	            mat[0][j]=false;
+	        }
+	    }
+	    for (int i=1;i<=n;i++)
+	    {
+	        for (int j=1;j<=sum;j++)
+	        {
+	            if (arr[i-1]<=j)
+	            {
+	                mat[i][j]=mat[i-1][j] || mat[i-1][j-arr[i-1]];
+	            }
+	            else 
+	            {
+	                mat[i][j]=mat[i-1][j];
+	            }
+	        }
+	    }
+	    return mat[n-1][sum];
+	}
+}
